33cf5c22f80bcb5c8c754aff315ce988b8382c5d
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-20 19:21:20 +0300

    Set compileSdk to 37 required by SceneView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.ali12hhh.kidslearning"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.ali12hhh.kidslearning"
