f36264f1c81650eba9ee409616330059cf440bd9
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-20 14:08:39 +0300

    Add JVM unit test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,5 +32,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.8.5")
+    testImplementation("junit:junit:4.13.2")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
