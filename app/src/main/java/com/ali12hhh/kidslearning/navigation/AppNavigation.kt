015270c1c0de5b8f48d53a186e933a8bd56bc434
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-23 22:37:28 +0300

    Connect English level 2 handwriting lessons

--- app/src/main/java/com/ali12hhh/kidslearning/navigation/AppNavigation.kt
+++ app/src/main/java/com/ali12hhh/kidslearning/navigation/AppNavigation.kt
@@ -179,12 +179,16 @@ fun AppNavigation() {
                 onSelectSection = { section ->
                     if (level == 1 && (section == "letters" || section == "numbers")) {
                         navController.navigate(if (section == "letters") AppRoutes.ENGLISH_LEVEL_ONE_LETTERS else AppRoutes.ENGLISH_LEVEL_ONE_NUMBERS)
+                    } else if (level == 2 && (section == "letters" || section == "numbers")) {
+                        navController.navigate(if (section == "letters") AppRoutes.ENGLISH_LEVEL_TWO_LETTERS else AppRoutes.ENGLISH_LEVEL_TWO_NUMBERS)
                     }
                 }
             )
         }
         composable(AppRoutes.ENGLISH_LEVEL_ONE_LETTERS) { EnglishLevelOnePage(onBack = { navController.popBackStack() }, initialSection = 0) }
         composable(AppRoutes.ENGLISH_LEVEL_ONE_NUMBERS) { EnglishLevelOnePage(onBack = { navController.popBackStack() }, initialSection = 1) }
+        composable(AppRoutes.ENGLISH_LEVEL_TWO_LETTERS) { EnglishLevelTwoPage(onBack = { navController.popBackStack() }, initialSection = 0) }
+        composable(AppRoutes.ENGLISH_LEVEL_TWO_NUMBERS) { EnglishLevelTwoPage(onBack = { navController.popBackStack() }, initialSection = 1) }
         composable(AppRoutes.NUMBERS) { ContentPage("الأرقام والعدّ", LearningCatalog.digits.joinToString("  ")) }
         composable(AppRoutes.PLAY) { ContentPage("وقت اللعب", "منطقة الألعاب قيد التجهيز") }
     }
