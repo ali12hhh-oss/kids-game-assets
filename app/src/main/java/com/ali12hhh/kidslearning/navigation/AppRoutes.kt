2f99f91a5f1e8632ff5dc5bd7baec08a54164e08
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-23 21:32:30 +0300

    Add direct English level 1 section routes

--- app/src/main/java/com/ali12hhh/kidslearning/navigation/AppRoutes.kt
+++ app/src/main/java/com/ali12hhh/kidslearning/navigation/AppRoutes.kt
@@ -16,7 +16,8 @@ object AppRoutes {
     const val ARABIC_LEVEL_TWO_MATH = "arabic_level_two_math"
     const val ARABIC_LEVEL_THREE_READING = "arabic_level_three_reading"
     const val ARABIC_LEVEL_THREE_MATH = "arabic_level_three_math"
-    const val ENGLISH_LEVEL_ONE = "english_level_one"
+    const val ENGLISH_LEVEL_ONE_LETTERS = "english_level_one_letters"
+    const val ENGLISH_LEVEL_ONE_NUMBERS = "english_level_one_numbers"
     const val NUMBERS = "numbers"
     const val PLAY = "play"
 }
