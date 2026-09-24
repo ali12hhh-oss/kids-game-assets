8ddb8bae0cc7693388659fa740d4fa9f0ae4e9ec
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-24 14:08:59 +0300

    Upgrade lesson buttons to professional 3D style

--- app/src/main/java/com/ali12hhh/kidslearning/navigation/EnglishLevelThreePage.kt
+++ app/src/main/java/com/ali12hhh/kidslearning/navigation/EnglishLevelThreePage.kt
@@ -1,5 +1,7 @@
 package com.ali12hhh.kidslearning.navigation
 
+import com.ali12hhh.kidslearning.navigation.ProLessonButton
+
 import android.speech.tts.TextToSpeech
 import com.ali12hhh.kidslearning.navigation.LessonSpeech
 import androidx.compose.foundation.Canvas
