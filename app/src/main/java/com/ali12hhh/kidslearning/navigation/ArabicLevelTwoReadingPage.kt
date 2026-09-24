2c1ce5dde90a05f8dc2797a5f57797d37456879a
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-24 15:57:42 +0300

    Fix missing LaunchedEffect import

--- app/src/main/java/com/ali12hhh/kidslearning/navigation/ArabicLevelTwoReadingPage.kt
+++ app/src/main/java/com/ali12hhh/kidslearning/navigation/ArabicLevelTwoReadingPage.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
