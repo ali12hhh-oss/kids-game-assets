3569685c873079113e233488eff7dcf0de224d7d
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-24 15:41:21 +0300

    Fix Arabic reading letter click import

--- app/src/main/java/com/ali12hhh/kidslearning/navigation/ArabicReadingPage.kt
+++ app/src/main/java/com/ali12hhh/kidslearning/navigation/ArabicReadingPage.kt
@@ -13,6 +13,7 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
