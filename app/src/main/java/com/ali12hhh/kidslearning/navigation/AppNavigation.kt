d72fac85643f00e8e654227cbc6361240c6f720d
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-24 22:23:31 +0300

    Fix missing Compose key import

--- app/src/main/java/com/ali12hhh/kidslearning/navigation/AppNavigation.kt
+++ app/src/main/java/com/ali12hhh/kidslearning/navigation/AppNavigation.kt
@@ -34,6 +34,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.key
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.mutableIntStateOf
