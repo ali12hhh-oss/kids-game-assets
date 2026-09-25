f7af8f80e2a36d1866dd1dc399e1bfc41c3279ea
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-25 17:41:26 +0300

    Add AndroidX Core KTX for URI extension

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.core:core-ktx:1.19.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.11.0")
     implementation("androidx.navigation:navigation-compose:2.10.2")
     implementation("io.coil-kt:coil-compose:2.7.0")
