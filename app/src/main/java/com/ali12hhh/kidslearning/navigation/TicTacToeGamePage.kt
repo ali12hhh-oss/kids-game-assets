76c42cf1defba009c867cd636c6b5ea76f6bfbd9
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-25 19:16:57 +0300

    Fix XO animation compile imports

--- app/src/main/java/com/ali12hhh/kidslearning/navigation/TicTacToeGamePage.kt
+++ app/src/main/java/com/ali12hhh/kidslearning/navigation/TicTacToeGamePage.kt
@@ -1,6 +1,7 @@
 package com.ali12hhh.kidslearning.navigation
 
 import android.content.Context
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.RepeatMode
 import androidx.core.content.edit
 import androidx.compose.animation.core.animateFloat
@@ -11,6 +12,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -40,6 +42,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
